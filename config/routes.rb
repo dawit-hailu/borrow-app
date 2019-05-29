3367fe96330f62f0805991dbe69ec3f40505b4cd
dawit <davucan@gmail.com>
2019-05-29 15:44:20 -0700

    create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :sessions
+
+  resources :users do
+    resources :uploads
+  end
+
+  get 'cities/:state', to: 'application#cities'
+
+  get '/register' => 'users#create'
+
+  get '/upload' => 'uploads#new'
+
+  get '/login' => 'sessions#new'
+
+  post  '/login' => 'sessions#create'
+
+  get '/logout', to: 'sessions#destroy'
+
+  get '/', to: 'welcome#index'
+
+  root 'welcome#index'
 end
